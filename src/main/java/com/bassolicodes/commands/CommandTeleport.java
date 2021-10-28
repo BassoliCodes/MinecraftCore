f54a18345b0f80e5d75a448be55690776c650e71
Lucca Bassoli <lucca.bassoli2020@gmail.com>
2021-10-28 20:28:46 -0300

    update

--- src/main/java/com/bassolicodes/commands/CommandTeleport.java
+++ src/main/java/com/bassolicodes/commands/CommandTeleport.java
@@ -1,10 +1,12 @@
 package com.bassolicodes.commands;
 
+import com.bassolicodes.MinecraftCore;
 import lombok.val;
 import me.saiintbrisson.minecraft.command.annotation.Command;
 import me.saiintbrisson.minecraft.command.command.Context;
 import me.saiintbrisson.minecraft.command.target.CommandTarget;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
@@ -18,13 +20,15 @@ public class CommandTeleport {
     public void handleCommand(Context<CommandSender> context, Player target) {
         val player = (Player) context.getSender();
 
+        FileConfiguration config = MinecraftCore.getInstance().getConfig();
+
         if (target == player) {
-            player.sendMessage("§cVocê não pode ir até si mesmo.");
+            player.sendMessage(config.getString("Message.Teleport_To_Yourself").replace("&", "§"));
         } else if (target == null) {
-            player.sendMessage("§cEste jogador não está online.");
+            player.sendMessage(String.format(config.getString("Message.Teleport_Player_Offline").replace("&", "§"), target.getName()));
         } else {
             player.teleport(target.getLocation(), PlayerTeleportEvent.TeleportCause.COMMAND);
-            player.sendMessage("§aVocê foi até a localização de " + target.getName() + ".");
+            player.sendMessage(String.format(config.getString("Message.Teleported_To_Player").replace("&", "§"), target.getName()));
         }
     }
 
